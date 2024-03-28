05e8cde4bdbe1284b25954de925c20d42a2dd0e9
Mathias Rönnmark <mathiasronnmark@hotmail.com>
2024-03-28 10:12:41 +0100

    Add project files.

---
+++ Infrastructure/Entities/SubscriberEntity.cs
@@ -0,0 +1,31 @@
+﻿using Infrastructure.Dtos;
+using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Entities;
+
+public class SubscriberEntity
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    [Required]
+    public string Email { get; set; } = null!;
+    public bool DailyNewsletter { get; set; }
+    public bool AdvertisingUpdates { get; set; }
+    public bool WeekinReview { get; set; }
+    public bool EventUpdates { get; set; }
+    public bool StartupsWeekly { get; set; }
+    public bool Podcasts { get; set; }
+
+    public static implicit operator SubscriberEntity(Subscriber dto)
+    {
+        return new SubscriberEntity
+        {
+            Email = dto.Email,
+            DailyNewsletter = dto.DailyNewsletter,
+            AdvertisingUpdates = dto.AdvertisingUpdates,
+            WeekinReview = dto.WeekinReview,
+            EventUpdates = dto.EventUpdates,
+            StartupsWeekly = dto.StartupsWeekly,
+            Podcasts = dto.Podcasts
+        };
+    }
+}
---
+++ SiliconWebApi/Controllers/SubscribersController.cs
@@ -0,0 +1,56 @@
+﻿using Infrastructure.Contexts;
+using Infrastructure.Dtos;
+using Infrastructure.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Xml;
+
+namespace SiliconWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubscribersController(DataContext context) : ControllerBase
+    {
+        private readonly DataContext _context = context;
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Subscriber dto)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!await _context.Subscribers.AnyAsync(x => x.Email == dto.Email))
+                {
+                    try
+                    {
+                        _context.Subscribers.Add(dto);
+                        await _context.SaveChangesAsync();
+                        return Created("", null);
+                    }
+                    catch
+                    {
+                        return Problem("Unable to create subscription");
+                    }
+                }
+
+                return Conflict("Your email address is already subscribed");
+            }
+
+            return BadRequest();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(Subscriber dto)
+        {
+            var subscriber = await _context.Subscribers.FirstOrDefaultAsync(x => x.Email == dto.Email);
+            if (subscriber != null)
+            {
+                _context.Subscribers.Remove(subscriber);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+
+            return NotFound();
+        }
+    }
+}
